db83192ef77014ae20491e29a6df2539c605d075
Anderson <anderson.msfisico@gmail.com>
2020-03-05 21:22:31 -0300

    Cadastrar um frabricante

--- app/controllers/manufacturers_controller.rb
+++ app/controllers/manufacturers_controller.rb
@@ -1,7 +1,30 @@
 class ManufacturersController < ApplicationController
 
     def index
-        
+        @manufacturers = Manufacturer.all
+    end
+
+    def new
+        @manufacturer = Manufacturer.new
+    end
+
+    def create
+        @manufacturer = Manufacturer.new(manufacturer_params)
+        if @manufacturer.save
+            redirect_to @manufacturer
+        else
+            render :new
+        end
+    end
+
+    def show
+        @manufacturer = Manufacturer.find(params[:id])
+    end
+
+    private
+
+    def manufacturer_params
+        params.require(:manufacturer).permit(:name)
     end
 
 end
